162e389f59302d3a86b18d2cbc5d6120c9811da4
goldmund <matthewtzito@gmail.com>
2020-12-29 20:43:34 -0800

    refactor: align formatting

--- example.js
+++ example.js
@@ -1,45 +1,45 @@
 const maybeHas = require('./lib');
 
 const obj = {
-	anchor: {
-		anchor: {
-			anchor: 0
-		}
-	},
-	bank: 2,
-	case: {
-		anchor: {
-			bank: {
-				anchor: 99
-			}
-		}
-	}
+  anchor: {
+    anchor: {
+      anchor: 0
+    }
+  },
+  bank: 2,
+  case: {
+    anchor: {
+      bank: {
+        anchor: 99
+      }
+    }
+  }
 };
 
 const objHas = maybeHas(obj);
 
 [
-	objHas('anchor.anchor.anchor'),
-	objHas('case.anchor.bank.anchor'),
-	objHas('case'),
-	objHas('does.not.exist')
+  objHas('anchor.anchor.anchor'),
+  objHas('case.anchor.bank.anchor'),
+  objHas('case'),
+  objHas('does.not.exist')
 ].forEach(output => console.log(output));
 
 const apiResponse = {
-	status: 200,
-	statusText: '',
-	data: {
-		data: {
-			token: 'base64',
-			guid: 'something-here'
-		}
-	}
+  status: 200,
+  statusText: '',
+  data: {
+    data: {
+      token: 'base64',
+      guid: 'something-here'
+    }
+  }
 };
 
 (async function getToken() {
-	const response = await Promise.resolve(apiResponse);
+  const response = await Promise.resolve(apiResponse);
 
-	if (maybeHas(response)('data.data.token'))
-		console.log(response.data.data.token);
-	else console.log('token not extant');
+  if (maybeHas(response)('data.data.token'))
+    console.log(response.data.data.token);
+  else console.log('token not extant');
 })();
